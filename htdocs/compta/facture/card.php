3d91a0b502881c749ce303ffc5d589023436c5e5
Amael-PE <amael@amael.io>
2025-02-28 20:59:19 +0100

    Feat Autonaming in invoices

--- htdocs/compta/facture/card.php
+++ htdocs/compta/facture/card.php
@@ -3322,6 +3322,9 @@ if ($action == 'create') {
 
 			$projectid = (!empty($projectid) ? $projectid : $objectsrc->fk_project);
 			$ref_client = (!empty($objectsrc->ref_client) ? $objectsrc->ref_client : (!empty($objectsrc->ref_customer) ? $objectsrc->ref_customer : ''));
+			// -- Hook Couffignal
+			$ref_client = 'S' . $objectsrc->situation_cycle_ref + 1;
+			// --
 
 			// only if socid not filled else it's allready done upper
 			if (empty($socid)) {
