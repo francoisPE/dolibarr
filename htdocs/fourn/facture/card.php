c81fbc046b159eab1dc4f05efe9002beb2e0fe90
francoisPE <60675606+francoisPE@users.noreply.github.com>
2026-05-31 09:57:01 +0200

    FEAT: card.php
    Ajoute la possibilité de modifier le tiers au valideur de facture (conduc)
    
    Signed-off-by: francoisPE <60675606+francoisPE@users.noreply.github.com>

--- htdocs/fourn/facture/card.php
+++ htdocs/fourn/facture/card.php
@@ -135,6 +135,7 @@ $usercansend = (!getDolGlobalString('MAIN_USE_ADVANCED_PERMS') || $user->hasRigh
 $permissiondellink = $usercancreate; // Used by the include of actions_dellink.inc.php
 $permissiontoedit = $usercancreate; // Used by the include of actions_lineupdown.inc.php
 $permissiontoadd = $usercancreate; // Used by the include of actions_addupdatedelete.inc.php and actions_lineupdown.inc.php
+if (getDolGlobalString('MAIN_FACTUREFOURN_VALIDATE_SUPERSEED_CREATE')) {$permissiontoadd = $usercanvalidate ; } else { $permissiontoadd = $usercancreate; }
 
 $error = 0;
 
@@ -1950,6 +1951,7 @@ if (empty($reshook)) {
 	// Actions to build doc
 	$upload_dir = $conf->fournisseur->facture->dir_output;
 	$permissiontoadd = $usercancreate;
+	if (getDolGlobalString('MAIN_FACTUREFOURN_VALIDATE_SUPERSEED_CREATE')) {$permissiontoadd = $usercanvalidate ; } else { $permissiontoadd = $usercancreate; }
 	include DOL_DOCUMENT_ROOT.'/core/actions_builddoc.inc.php';
 
 	// Make calculation according to calculationrule
